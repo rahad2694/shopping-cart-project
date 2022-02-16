3eb39fdbb49509e063d7620d531ae58cfacf6da2
rahad2694 <rahad.haw@gmail.com>
2022-02-16 19:09:07 +0600

    cupon option added

--- js/app.js
+++ js/app.js
@@ -29,9 +29,21 @@ function updatePriceAndQuant(itemName,targetChangeBox, price,isadd){
 const perBurger = 30;
 const perHotDog = 20;
 const perCoffee = 10;
+const cuponCode = 'PHero';
 updatePriceAndQuant('burger','burger-quant',perBurger,true);
 updatePriceAndQuant('burger','burger-quant',perBurger,false);
 updatePriceAndQuant('hot-dog','hot-dog-quant',perHotDog,true);
 updatePriceAndQuant('hot-dog','hot-dog-quant',perHotDog,false);
 updatePriceAndQuant('coffee','coffee-quant',perCoffee,true);
 updatePriceAndQuant('coffee','coffee-quant',perCoffee,false);
+
+document.getElementById('cupon-btn').addEventListener('click',function(){
+    const cuponInputBox = document.getElementById('cupon-input');
+    if(cuponInputBox.value === cuponCode){
+        document.getElementById('total-price').innerText= Number(document.getElementById('total-price').innerText)- Number(document.getElementById('total-price').innerText) * .2;
+
+        document.getElementById('cupon-btn').setAttribute('disabled',true);
+    } else{
+        console.log('wrong!');
+    }
+})
